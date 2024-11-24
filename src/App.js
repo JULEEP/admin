e4d16992306ab46e7c4bca7dc5fe4690af65f328
JuleeP <juleeperween34@gmail.com>
2024-11-23 20:16:48 -0800

    chal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from './utils/toast';
+import AccessibleNavigationAnnouncer from './components/AccessibleNavigationAnnouncer';
 
-function App() {
+// Importing pages
+import Dashboard from './pages/Dashboard';
+import Customers from './pages/Customers';
+import EditProfile from './pages/EditProfile';
+import Page404 from './pages/404';
+import ProductsPage from './pages/ProductPage';
+import CustomerPage from './pages/CustomerPage';
+import StaffPage from './pages/StaffPage';
+import OrderPage from './pages/OrderPage';
+import ProductDetails from './pages/ProductDetails';
+import UpdateProduct from './pages/UpdateProduct';
+import UpdateStaff from './components/staff/UpdateStaff';
+import CategoryPage from './pages/CategoryPage';
+import CreateProduct from './components/product/CreateProduct';
+import UpdateCategoryPage from './components/category/UpdateCategoryPage';
+import AddCategoryPage from './components/category/AddCategoryPage';
+import AddStaff from './components/staff/AddStaff';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer />
+      <Router>
+        <AccessibleNavigationAnnouncer />
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/get-products" element={<ProductsPage />} />
+          <Route path="/customers" element={<CustomerPage />} />
+          <Route path="/product-details/:id" element={<ProductDetails />} />
+          <Route path="/update-product/:id" element={<UpdateProduct />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/staffs" element={<StaffPage />} />
+          <Route path="/orders" element={<OrderPage />} />
+          <Route path="/add-product" element={<CreateProduct />} />
+          <Route path="/add-staff" element={<AddStaff />} />
+          <Route path="/update-staff/:id" element={<UpdateStaff />} />
+          <Route path="/categories" element={<CategoryPage />} />
+          <Route path="/update-category/:id" element={<UpdateCategoryPage />} />
+          <Route path="/add-category" element={<AddCategoryPage />} />
+          <Route path="/setting" element={<EditProfile />} />
+          <Route path="/404" element={<Page404 />} />
+          <Route path="/" element={<SignUp />} />
+          <Route path="/admin-login" element={<SignIn />} />
+          <Route path="/edit-profile" element={<EditProfile />} />
+          {/* Catch-all for undefined routes */}
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import './assets/css/custom.css';
+import './assets/css/tailwind.css';
+import './assets/css/tailwind.output.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ThemeSuspense from './components/theme/ThemeSuspense';
+// import * as serviceWorker from './serviceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// if (process.env.NODE_ENV !== "production") {
+//   const axe = require("react-axe");
+//   axe(React, ReactDOM, 1000);
+// }
+
+ReactDOM.render(
+  <Suspense fallback={<ThemeSuspense />}>
     <App />
-  </React.StrictMode>
+  </Suspense>,
+  document.getElementById('root') // This is where your React app will be mounted
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.register();
